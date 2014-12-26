daecb02c6bd5e3352c48bf61e3a6845351bc659c
Pranav Kulkarni <pranavcode@gmail.com>
2014-12-26 23:29:11 +0530

    [NEURON] Adding HH Neuron for HH model.

---
+++ neuron/N_StellateCell_HH1952.hpp
@@ -0,0 +1,59 @@
+/*
+ neuron/N_StellateCell_HH1952.hpp - Hodgkin-Huxley Stellate cell (Hodgkin-Huxley, 1952)
+
+ Copyright (C) 2014 Pranav Kulkarni, Collins Assisi Lab, IISER, Pune <pranavcode@gmail.com>
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef INCLUDED_N_STELLATECELL_HH9152_HPP
+#define INCLUDED_N_STELLATECELL_HH9152_HPP
+
+#include "core/engine.hpp"
+
+#include "current/I_Na_HH1952.hpp"
+#include "current/I_K_HH1952.hpp"
+#include "current/I_Leak_HH1952.hpp"
+
+namespace insilico {
+
+class N_StellateCell_HH1952 {
+ public:
+  static void ode_set(const state_type &variables, state_type &dxdt, const double t,
+               int index) {
+    int v_index = engine::neuron_index(index, "v");
+
+    float v = variables[v_index];
+      
+    vector<int> g1_indices = engine::get_pre_neuron_indices(index, "g1");
+    vector<int> esyn_values = engine::get_pre_neuron_values(index, "esyn");
+
+    float I_Syn = 0;
+    for(vector<int>::size_type iterator = 0; iterator < g1_indices.size(); ++iterator) {
+      I_Syn = I_Syn + variables[g1_indices[iterator]] * (v - esyn_values[iterator]);
+    }
+
+    float I_Na = I_Na_HH1952::current(variables, dxdt, t, index);
+    float I_K = I_K_HH1952::current(variables, dxdt, t, index);
+    float I_Leak = I_Leak_HH1952::current(variables, dxdt, t, index);
+    float I_Ext = engine::neuron_value(index, "iext");
+    
+    // ODE set
+    dxdt[v_index] = I_Na + I_K + I_Leak + I_Ext + I_Syn;
+  } // function ode_set
+}; // class N_StellateCell_HH1952
+
+} // insilico
+
+#endif
