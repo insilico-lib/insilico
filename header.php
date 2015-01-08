3b103058ece8b9590bcbdc05cca6030b685546ae
Pranav Kulkarni <pranavcode@gmail.com>
2015-01-08 14:38:06 +0530

    [Web] Adding header page. Dependent.

---
+++ header.php
@@ -0,0 +1,5 @@
+<link href="style.css" rel="stylesheet" type="text/css">
+<?php
+echo '<p style="margin: 0 auto; padding: 10px;"><a style="color: #09648b;; font: 35px monospace, courier bold; padding: 10px;" href="index.php">insilico</a>';
+echo 'Neuronal Simulation Library</p>';
+?>
