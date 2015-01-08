1f50a1632f9370c2da40e43dff418015bbe996da
Pranav Kulkarni <pranavcode@gmail.com>
2015-01-08 14:38:43 +0530

    [Web] Adding index page. Dependent.

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php include_once("pre_wrapper.php"); ?>
+
+<p class="page-content-header">Welcome to <span class="lib-name-font">insilico</span>!</p>
+
+<p class="page-contents-value">
+    <span class="lib-name-font">insilico</span> is a simulation library for Computational Neuroscience simulations written in C++ programming language.
+    <br/><br/>
+    <span class="lib-name-font">insilico</span> encourages ready-to-start approach for quick setup and development of simulation environment, without hindering programmers time and focus from intended experiment.
+</p>
+
+<?php include_once("post_wrapper.php"); ?>
