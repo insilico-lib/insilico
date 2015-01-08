73b718d4b10e35b842a322c84fb4dbec914207ab
Pranav Kulkarni <pranavcode@gmail.com>
2015-01-08 14:39:24 +0530

    [Web] Adding download page. Dependent.

---
+++ download.php
@@ -0,0 +1,30 @@
+<?php include_once("pre_wrapper.php"); ?>
+
+<p class="page-content-header">Download</p>
+
+<p style="text-align: center; padding: 10px;"><a href="get_insilico"><button style="font-size: 30px; border-radius: 10px;">Get insilico 0.10</button></a></p>
+
+<p style="padding-left: 10px;">
+    <span>Current stable release:</span><b> <a href="get_insilico">Version 0.10</a></b> (January 3, 2015)
+</p>
+<p style="padding-left: 10px;">
+    <span>Unstable release:</span><b> <a href="get_old_insilico">Version 0.11</a></b> (in developement)
+</p>
+<p style="padding-left: 10px;">
+    <span>Significant updates in current stable release:</span>
+    <ul>
+    <li> Generalized code compilation strategy</li>
+    <li> Enhancement to code execution strategy</li>
+    <li> Improved and concise sample example</li>
+    <li> Simulation specific concepts - Configuration and Engine</li>
+    <li> Community support improvement</li>
+    </ul>
+</p>
+<p style="padding-left: 10px;">
+    <span>Previous Releases:</span>
+    <ul>
+    <li><a href="get_old_insilico">Version 0.02</a> (December 3, 2014)</li>
+    <li><a href="get_old_insilico">Version 0.01</a> (November 29, 2014)</li>
+    </ul>    
+</p>    
+<?php include_once("post_wrapper.php"); ?>
