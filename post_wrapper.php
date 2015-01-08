aa5af55e56c502d7f87be807ded30978ef8194f2
Pranav Kulkarni <pranavcode@gmail.com>
2015-01-08 14:41:45 +0530

    [Web] Abstracting the pre and post ceremony for most documentation pages into pre and post wrappers. Dependent.

---
+++ post_wrapper.php
@@ -0,0 +1,4 @@
+<?php
+include_once("footer.php");
+echo "</div>"
+?>
---
+++ pre_wrapper.php
@@ -0,0 +1,7 @@
+<?php
+include_once("doctype_decl.php");
+
+echo '<div class="wrapper">';
+include_once("header.php");
+include_once("menu.php");
+?>
