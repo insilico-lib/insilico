70666c3045675e48de487633bef4575c0caee12e
Pranav Kulkarni <pranavcode@gmail.com>
2015-01-08 15:56:38 +0530

    [Web] Chrome compatibility visual tweaks.

--- web/footer.php
+++ web/footer.php
@@ -1,3 +1,3 @@
 <?php
-  echo '<p style="font-size: 10px;border-top: 0.5px solid #3388ac; margin: 0 auto; padding: 10px">Copyright (C) 2015 Collins Assisi</p>';
+  echo '<p style="font-size: 10px;border-top: 1px solid #3388ac; margin: 0 auto; padding: 10px">Copyright &copy; 2015 Collins Assisi</p>';
 ?>
