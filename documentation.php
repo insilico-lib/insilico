0e300eb45269be5cb091cab22f4f1f9c399355c0
Pranav Kulkarni <pranavcode@gmail.com>
2015-01-08 14:39:51 +0530

    [Web] Adding documentation page. Dependent.

---
+++ documentation.php
@@ -0,0 +1,31 @@
+<?php include_once("pre_wrapper.php"); ?>
+
+<p class="page-content-header">Documentation</p>
+<!-- <p style="margin-left: 10px; padding-left: 10px;">
+   &bull; Offline version of this documentation - <a href="get_doc_pdf"><span class="lib-name-font">insilico</span> 0.10</a>
+   </p> -->
+<p style="margin: 0px; text-align: right; float: right; padding-right: 10px;">
+    &lArr;
+    <a href="documentation.php">&uArr;</a>
+    <a href="introduction.php">&rArr;</a>
+</p>
+<p>
+    <table style="text-align: legt; margin: 20px; padding: 10px; border: 0.5px dotted #3388ac;">
+	<tr><td><u>Contents</u></td></tr>
+	<?php
+	$contentlist = array("Introduction", 
+			     "Getting Started",
+			     "Tutorial",
+			     "In Detail",
+			     "Contribute",
+			     "Acknowledgements",
+			     "Code Reference",
+			     "Library Index");
+	foreach($contentlist as $contentitem) {
+	    echo '<tr><td><a href="'.(str_replace(" ","_",strtolower($contentitem))).'.php">'.$contentitem.'</td></tr>'; 
+	}
+	?>
+    </table>
+</p>
+
+<?php include_once("post_wrapper.php"); ?>
