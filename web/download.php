d0aa30efeff6dd0af9a23b4b374fcae2c827c1c7
Pranav Kulkarni <pranavcode@gmail.com>
2015-01-09 19:15:54 +0530

    [web] URLs for download page works now.

--- web/download.php
+++ web/download.php
@@ -2,14 +2,14 @@
 
 <p class="page-content-header">Download</p>
 
-<p style="text-align: center; padding: 10px;"><a href="get_insilico"><button style="font-size: 30px; border-radius: 10px;">Get insilico 0.10</button></a></p>
+<p style="text-align: center; padding: 10px;"><a href="insilico-0.10.zip"><button style="font-size: 30px; border-radius: 10px;">Get insilico 0.10</button></a></p>
 
 <p style="padding-left: 10px;">
-    <span>Current stable release:</span><b> <a href="get_insilico">Version 0.10</a></b> (January 3, 2015)
+    <span>Current stable release:</span><b> Version 0.10 </a></b> (January 3, 2015)
 </p>
-<p style="padding-left: 10px;">
+<!--<p style="padding-left: 10px;">
     <span>Unstable release:</span><b> <a href="get_old_insilico">Version 0.11</a></b> (in developement)
-</p>
+</p>-->
 <p style="padding-left: 10px;">
     <span>Significant updates in current stable release:</span>
     <ul>
@@ -21,10 +21,12 @@
     </ul>
 </p>
 <p style="padding-left: 10px;">
-    <span>Previous Releases:</span>
+    <span>Download links to previous versions:</span>
     <ul>
-    <li><a href="get_old_insilico">Version 0.02</a> (December 3, 2014)</li>
-    <li><a href="get_old_insilico">Version 0.01</a> (November 29, 2014)</li>
+    <li><a href="insilico-0.04">Version 0.04</a> (December 17, 2014)</li>
+    <li><a href="insilico-0.035">Version 0.035</a> (December 6, 2014)</li>
+    <li><a href="insilico-0.03">Version 0.03</a> (December 4, 2014)</li>
+    <li><a href="insilico-0.02">Version 0.02</a> (December 2, 2014)</li>
     </ul>    
 </p>    
 <?php include_once("post_wrapper.php"); ?>
