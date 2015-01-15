60ce1827d0050fc600ab4298d32b92a3e22a314c
Pranav Kulkarni <pranavcode@gmail.com>
2015-01-16 04:25:41 +0530

    [Error Handling] Errors revised for proper message.

--- core/configuration.hpp
+++ core/configuration.hpp
@@ -87,18 +87,14 @@ class configuration {
 
   static void initialize(int argc, char** argv) {
     if(argc < 3 || argc > 4) {
-      cout<<"[insilico/configuration/initialize] USAGE: "<<argv[0]
-          <<" <output_file>.dat <neuron_file>.conf [<synapse_file>.conf]"<<endl
-          <<"Suggestion: Check file count."
-          <<endl;
+      cout<<"[insilico::configuration::initialize] USAGE: "<<argv[0]
+          <<" <output_file>.dat <neuron_file>.conf [<synapse_file>.conf]"<<endl;
       exit(0);
     }
     cout<<endl<<"[insilico/configuration/initialize] SUCCESS: Initializing with following parameters:"<<endl
         <<"Output file: "<<argv[1]<<endl<<"Neuron file: "<<argv[2]<<endl;
-
     outstream.open(argv[1], ios::out);
     string neuron_file(argv[2]);
-
     if(argc == 4) {
       cout<<"Synapse file: "<<argv[3]<<endl;
       string synapse_file(argv[3]);
