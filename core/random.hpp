d75348002641e2f5dcb09a61b144d11b27ee869a
Pranav Kulkarni <pranavcode@gmail.com>
2015-01-16 05:39:53 +0530

    [RNG] insilico now has its own RNG (random number generator) function API.

---
+++ core/random.hpp
@@ -0,0 +1,43 @@
+/*
+ core/random.hpp - insilico's own RNG (random number generator) header and source
+
+ Copyright (C) 2015 Pranav Kulkarni, Collins Assisi Lab, IISER, Pune <pranavcode@gmail.com>
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef INCLUDED_RANDOM_HPP
+#define INCLUDED_RANDOM_HPP
+
+#include <random>
+
+using namespace insilico;
+using namespace std;
+
+namespace insilico {
+
+class random {
+ public:  
+  template<class T>
+  static T rand(T min, T max) {
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_real_distribution<> dist(min, max+1);
+    return dist(gen);
+  }
+};
+
+}
+
+#endif
