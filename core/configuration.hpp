595431ca1bf506e8eeb89b28025be416d413c890
Pranav Kulkarni <pranavcode@gmail.com>
2015-01-17 03:15:42 +0530

    [Format] Method def end does not require comment.

--- core/configuration.hpp
+++ core/configuration.hpp
@@ -49,7 +49,7 @@ class configuration {
   static inline std::string &ltrim(std::string &s) {
     s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
     return s;
-  } // function ltrim
+  }
 
   // trim from end
   static inline std::string &rtrim(std::string &s) {
