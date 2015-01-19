0fdb732d2c725b4d599d106c42f751a2aee8fc79
Pranav Kulkarni <pranavcode@gmail.com>
2015-01-19 17:22:12 +0530

    [0.15 Released] Download page updated for version 0.15.

--- web/download.php
+++ web/download.php
@@ -2,31 +2,35 @@
 
 <p class="page-content-header">Download</p>
 
-<p style="text-align: center; padding: 10px;"><a href="insilico-0.10.zip"><button style="font-size: 30px; border-radius: 10px;">Get insilico 0.10</button></a></p>
+<p style="text-align: center; padding: 10px;"><a href="insilico-0.15.zip"><button style="font-size: 35px; border-radius: 20px;">&downarrow; insilico 0.15</button></a></p>
 
 <p style="padding-left: 10px;">
-    <span>Current stable release:</span><b> Version 0.10 </a></b> (January 3, 2015)
+    <span>Current stable release:</span><b> Version 0.15 </a></b> (January 19, 2015)
 </p>
-<!--<p style="padding-left: 10px;">
-    <span>Unstable release:</span><b> <a href="get_old_insilico">Version 0.11</a></b> (in developement)
-</p>-->
 <p style="padding-left: 10px;">
     <span>Significant updates in current stable release:</span>
     <ul>
-    <li> Generalized code compilation strategy</li>
-    <li> Enhancement to code execution strategy</li>
-    <li> Improved and concise sample example</li>
-    <li> Simulation specific concepts - Configuration and Engine</li>
-    <li> Community support improvement</li>
+    <li>Supporting comments ("") in input files <small style="color:green;">(new)</small></li>
+    <li>Current values can be stored and retrieved independently <small style="color:green;">(new)</small></li>
+    <li>Multi-line inputs for single neuron/synapse are supported <small style="color:green;">(new)</small></li>
+    <li>Update to constant values in file is now permittable <small style="color:green;">(new)</small></li>
+    <li>Double precision floating points for better precision in computations <small style="color:green;">(new)</small></li>
+    <li>insilico has its own RNG - Random Number Generator <small style="color:green;">(new)</small></li>
+    <li>NAMING_CONVENTION document added, Contribution and README improved</li>
+    <li>Web portal improved and part of library source</li>
+    <li>Input file parsing is faster and cleaner</li>
+    <li>Logic for delay in synapse and spike duration in neuron improved</li>
+    <li>Example code improvement</li>
     </ul>
 </p>
 <p style="padding-left: 10px;">
     <span>Download links to previous versions:</span>
     <ul>
-    <li><a href="insilico-0.04">Version 0.04</a> (December 17, 2014)</li>
-    <li><a href="insilico-0.035">Version 0.035</a> (December 6, 2014)</li>
-    <li><a href="insilico-0.03">Version 0.03</a> (December 4, 2014)</li>
-    <li><a href="insilico-0.02">Version 0.02</a> (December 2, 2014)</li>
+    <li><a href="insilico-0.10.zip">Version 0.10</a> (January 3, 2015)</li>
+    <li><a href="insilico-0.04.zip">Version 0.04</a> (December 17, 2014)</li>
+    <li><a href="insilico-0.035.zip">Version 0.035</a> (December 6, 2014)</li>
+    <li><a href="insilico-0.03.zip">Version 0.03</a> (December 4, 2014)</li>
+    <li><a href="insilico-0.02.zip">Version 0.02</a> (December 2, 2014)</li>
     </ul>    
 </p>    
 <?php include_once("post_wrapper.php"); ?>
