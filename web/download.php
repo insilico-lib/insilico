1849ea4aa382020e310621925a08c729df6f838b
Pranav Kulkarni <pranavcode@gmail.com>
2015-01-21 19:55:29 +0530

    [Release 0.16] Update web/download page.

--- web/download.php
+++ web/download.php
@@ -2,30 +2,23 @@
 
 <p class="page-content-header">Download</p>
 
-<p style="text-align: center; padding: 10px;"><a href="insilico-0.15.zip"><button style="font-size: 35px; border-radius: 20px;">&downarrow; insilico 0.15</button></a></p>
+<p style="text-align: center; padding: 10px;"><a href="insilico-0.16.zip"><button style="font-size: 35px; border-radius: 20px;">&downarrow; insilico 0.16</button></a></p>
 
 <p style="padding-left: 10px;">
-    <span>Current stable release:</span><b> Version 0.15 </a></b> (January 19, 2015)
+    <span>Current stable release:</span><b> Version 0.16 </a></b> (January 21, 2015)
 </p>
 <p style="padding-left: 10px;">
     <span>Significant updates in current stable release:</span>
     <ul>
-    <li>Supporting comments ("") in input files <small style="color:green;">(new)</small></li>
-    <li>Current values can be stored and retrieved independently <small style="color:green;">(new)</small></li>
-    <li>Multi-line inputs for single neuron/synapse are supported <small style="color:green;">(new)</small></li>
-    <li>Update to constant values in file is now permittable <small style="color:green;">(new)</small></li>
-    <li>Double precision floating points for better precision in computations <small style="color:green;">(new)</small></li>
-    <li>insilico has its own RNG - Random Number Generator <small style="color:green;">(new)</small></li>
-    <li>NAMING_CONVENTION document added, Contribution and README improved</li>
-    <li>Web portal improved and part of library source</li>
-    <li>Input file parsing is faster and cleaner</li>
-    <li>Logic for delay in synapse and spike duration in neuron improved</li>
-    <li>Example code improvement</li>
+    <li>Engine::variables vector is writable <small style="color:green;">(new)</small></li>
+    <li>Simple Integrate and Fire neuron and example <small style="color:green;">(new)</small></li>
+    <li>Parser for input files improved</li>
     </ul>
 </p>
 <p style="padding-left: 10px;">
     <span>Download links to previous versions:</span>
     <ul>
+    <li><a href="insilico-0.15.zip">Version 0.15</a> (January 19, 2015)</li>
     <li><a href="insilico-0.10.zip">Version 0.10</a> (January 3, 2015)</li>
     <li><a href="insilico-0.04.zip">Version 0.04</a> (December 17, 2014)</li>
     <li><a href="insilico-0.035.zip">Version 0.035</a> (December 6, 2014)</li>
