1eaa7afbcd1554b8e29d5821c4ca84588caad4d0
Pranav Kulkarni <pranavcode@gmail.com>
2015-01-28 23:48:17 +0530

    [I/Engine] File inclusion macro change.

--- lib/core/engine.hpp
+++ lib/core/engine.hpp
@@ -17,8 +17,8 @@
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef INCLUDED_ENGINE_HPP
-#define INCLUDED_ENGINE_HPP
+#ifndef INCLUDED_INSILICO_ENGINE_HPP
+#define INCLUDED_INSILICO_ENGINE_HPP
 
 #include <algorithm>
 #include <cassert>
