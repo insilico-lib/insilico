b01cac98a2fd7828d9ca4aecd2aa62ccb4efad7a
Pranav Kulkarni <pranavcode@gmail.com>
2015-02-11 19:25:57 +0530

    [Release 0.17] Updating RELEASES and website's download page.

--- web/download.php
+++ web/download.php
@@ -3,22 +3,25 @@
 
 <p class="page-content-header">Download</p>
 
-<p style="text-align: center; padding: 10px;"><a href="insilico-0.16.zip"><button style="font-size: 35px; border-radius: 20px;">&downarrow; insilico 0.16</button></a></p>
+<p style="text-align: center; padding: 10px;"><a href="insilico-0.17.zip"><button style="font-size: 35px; border-radius: 20px;">&downarrow; insilico 0.17</button></a></p>
 
 <p style="padding-left: 10px;">
-    <span>Current stable release:</span><b> Version 0.16 </a></b> (January 21, 2015)
+    <span>Current stable release:</span><b> Version 0.17 </a></b> (February 11, 2015)
 </p>
 <p style="padding-left: 10px;">
     <span>Significant updates in current stable release:</span>
     <ul>
-    <li>Engine::variables vector is writable <small style="color:green;">(new)</small></li>
-    <li>Simple Integrate and Fire neuron and example <small style="color:green;">(new)</small></li>
-    <li>Parser for input files improved</li>
+	<li>Observation of constants from Neurons <small style="color:green;">(new feature)</small></li>
+	<li>Improved compilation time <small style="color:green;">(new)</small></li>
+	<li>Explicit include directory <small style="color:green;">(new)</small></li>
+	<li>Values transferred from connections (synapses) to nodes (neurons) are double precision floating points <small style="color:red;">(bug fix)</small></li>
+	<li>Connectivity file (a.k.a. synapse file) is optional <small style="color:red;">(bug fix)</small></li>
     </ul>
 </p>
 <p style="padding-left: 10px;">
     <span>Download links to previous versions:</span>
     <ul>
+    <li><a href="insilico-0.16.zip">Version 0.16</a> (January 21, 2015)</li>	
     <li><a href="insilico-0.15.zip">Version 0.15</a> (January 19, 2015)</li>
     <li><a href="insilico-0.10.zip">Version 0.10</a> (January 3, 2015)</li>
     <li><a href="insilico-0.04.zip">Version 0.04</a> (December 17, 2014)</li>
