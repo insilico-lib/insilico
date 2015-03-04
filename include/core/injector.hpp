074e8e4985cc14c16122a38d2004fdc5735a6c34
Pranav Kulkarni <pranavcode@gmail.com>
2015-03-04 10:48:01 +0530

    [Injector] Adding feature of injectable external current with new switch in simulator.

---
+++ include/core/injector.hpp
@@ -0,0 +1,46 @@
+/*
+ core/injector.hpp - insilico's external current injector header and source
+
+ Copyright (C) 2015 Pranav Kulkarni, Collins Assisi Lab, IISER, Pune <pranavcode@gmail.com>
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef INCLUDED_INSILICO_CORE_INJECTOR_HPP
+#define INCLUDED_INSILICO_CORE_INJECTOR_HPP
+
+#include "core/injector/parser.hpp"
+
+namespace insilico { namespace injector {
+
+std::vector<double> external_current(const int neuron_id, const double time) {
+  std::vector<double> current_values;
+  int index = (std::find(injector::time_seq.begin(), injector::time_seq.end(), time) - injector::time_seq.begin());
+  if(index > injector::time_seq.size()) {
+    std::cerr << "[insilico::injector] External current for neuron " << neuron_id
+              << " at time "<< time << " does not exist.\n";
+    exit(1);
+  }
+  
+  for(std::vector<int>::size_type id = 0; id < injector::neurons_seq.size(); ++id) {
+    if(neuron_id == injector::neurons_seq[id]) {
+      current_values.push_back(injector::external_current_seq[neuron_id][index]);
+    }    
+  }
+  return current_values;
+}
+
+} } // namespace insilico::injector
+  
+#endif
---
+++ include/core/injector/parser.hpp
@@ -0,0 +1,87 @@
+/*
+ core/injector/parser.hpp - insilico Current injector's parsing
+                            capability header and source
+
+ Copyright (C) 2015 Pranav Kulkarni, Collins Assisi Lab, IISER, Pune <pranavcode@gmail.com>
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef INCLUDED_INSILICO_CORE_INJECTOR_PARSER_HPP
+#define INCLUDED_INSILICO_CORE_INJECTOR_PARSER_HPP
+
+#include "core/helper/file.hpp"
+#include "core/helper/string.hpp"
+
+#include <fstream>
+#include <iostream>
+#include <cmath>
+#include <sstream>
+#include <string>
+#include <vector>
+
+namespace insilico { namespace injector {
+
+std::vector< std::vector<double> > external_current_seq;
+std::vector<double> time_seq;
+std::vector<int> neurons_seq;
+
+void read(const std::string &external_current_file)
+{
+  bool header;
+  char linedelim = '\n', worddelim = ',';
+  std::string part;
+  
+  std::ifstream extcurrfile_stream(external_current_file);
+  if(file_check(extcurrfile_stream, external_current_file)) {
+    while(getline(extcurrfile_stream, part, linedelim)) { // read a line
+      header = false;
+      std::stringstream l(part);
+      if((trim(part)).length() > 0) {
+        if(starts_with(part,"time")) {
+          header = true;
+        }
+        std::vector<double> seq;
+        while(getline(l, part, worddelim)) { // read the current
+          if((trim(part)).length() > 0) {
+            if(header == true) {
+              if(part.compare("time") == 0) continue;
+              int neuron_id = (int)(ceil(string_to_double(part)));
+              neurons_seq.push_back(neuron_id);
+            }
+            else {
+              if(neurons_seq.empty()) {
+                std::cerr << "[insilico::injector] External current input file is incorrect or malformed.";
+                exit(0);
+              }
+              seq.push_back(string_to_double(part));
+            }
+          }
+        }
+        if(!seq.empty()) {
+          time_seq.push_back(seq[0]);
+          external_current_seq.resize(neurons_seq.size());
+          for(std::vector<int>::size_type iter = 0; iter < neurons_seq.size(); ++iter) {
+            external_current_seq[iter].push_back(seq[iter+1]);
+          }
+        }
+      }
+    }
+  }
+  extcurrfile_stream.close();
+}
+
+} } // namespace insilico::injector
+
+#endif
