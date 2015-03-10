678eade010971b5a1e7a69d8dde04dd1462616cb
Pranav Kulkarni <pranavcode@gmail.com>
2015-03-10 12:44:32 +0530

    Script now supports new output format.

--- scripts/gnuplot_script.sh
+++ scripts/gnuplot_script.sh
@@ -1,11 +1,8 @@
-
 #!/bin/sh
 #
 # Script for continuous display of simulation output through Gnuplot
 #
-# Copyright (c) 2015 Pranav Kulkarni
-# This file is GNU GPL v3.0 licenced.
-#
+
 if [ $# -eq 0 ]
 then
    echo "Error: File name missing form command line argument."
@@ -14,15 +11,10 @@ then
 exit 1
 fi
 
-gnuplot -persist -e "set term jpeg; set output \"output_file_v.jpeg\";
-     set datafile separator \",\";
-     plot \""$1"\" using 1:2 with lines;"
-
-#gnuplot -persist -e "set term jpeg; set output \"output_file_others.jpeg\";
-#       set datafile separator \",\";
-#       plot \""$1"\" using 1:3 with lines title \"m\",
-#            \""$1"\" using 1:4 with lines title \"h\",
-#            \""$1"\" using 1:5 with lines title \"n\";"
+gnuplot -persist -e "set term jpeg;
+                     set key autotitle columnhead;
+                     set output \"output_file_v.jpeg\";
+                     set datafile separator \",\";
+                     plot \""$1"\" using 1:2 with lines;"
 
 open output_file_v.jpeg
-#open output_file_others.jpeg
