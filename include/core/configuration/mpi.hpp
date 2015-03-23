c98cc2b5cd38e4d4b941f0a4dd2d3a2a79b576c6
Pranav Kulkarni <pranavcode@gmail.com>
2015-03-24 03:26:32 +0530

    Remove unnecessary macro guards

--- include/core/configuration/mpi.hpp
+++ include/core/configuration/mpi.hpp
@@ -1,8 +1,7 @@
 /*
- core/configuration/mpi.hpp - insilico's MPI Configuration header and source
+ core/configuration/mpi.hpp - insilico's MPI Configuration source
 
- Copyright (C) 2014 Collins Assisi, Collins Assisi Lab, IISER, Pune
- Copyright (C) 2014-2015 Pranav Kulkarni, Collins Assisi Lab, IISER, Pune <pranavcode@gmail.com>
+ Copyright (C) 2015 Pranav Kulkarni, Collins Assisi Lab, IISER, Pune <pranavcode@gmail.com>
 
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
@@ -21,6 +20,7 @@
 #ifndef INCLUDED_INSILICO_INCLUDE_CORE_CONFIGURATION_MPI_HPP
 #define INCLUDED_INSILICO_INCLUDE_CORE_CONFIGURATION_MPI_HPP
 
+#include "core/configuration/error.hpp"
 #include "core/engine.hpp"
 #include "core/helper/file.hpp"
 #include "core/helper/string.hpp"
@@ -33,13 +33,9 @@ namespace insilico { namespace configuration {
 
 // initialization, check and handle commandline arguments
 void initialize(int argc, char **argv) {
-#ifdef INSILICO_MPI_ENABLE
   mpi::configuration::initialize(argc, argv);
-#endif
   if(argc < 3 || argc > 4) {
-#ifdef INSILICO_MPI_ENABLE
     if(mpi::rank == MASTER) {
-#endif
       std::string usage_error_msg = "[insilico::configuration::initialize] USAGE: ";
       usage_error_msg += argv[0];
       usage_error_msg += " -o<output_file.csv> -n<neuron_file.isf> -s<synapse_file.isf> -e<external_file.isfc>\n\n \
@@ -47,44 +43,28 @@ void initialize(int argc, char **argv) {
                            \t-e   External current configuration file (optional)\n             \
                            \nNo space allowed between option and its value.\n";
       std::cerr << usage_error_msg;
-#ifdef INSILICO_MPI_ENABLE
     }
     mpi::abort();
-#else
-    exit(1);
-#endif
   }
-#ifdef INSILICO_MPI_ENABLE
   if(insilico::mpi::rank == MASTER) {
-#endif
     std::cerr << "[insilico::configuration::initialize] SUCCESS: Initializing with following parameters:"<<'\n'
               << "Output file: " << argv[1] << '\n' << "Neuron file: " << argv[2] << '\n';
-#ifdef INSILICO_MPI_ENABLE
   }
-#endif
   outstream.open(argv[1], std::ios::out);
   std::string neuron_file(argv[2]);      
   if(argc == 4) {
-#ifdef INSILICO_MPI_ENABLE
     if(insilico::mpi::rank == MASTER) {
-#endif
       std::cerr << "Synapse file: " << argv[3] << '\n';
-#ifdef INSILICO_MPI_ENABLE
     }
-#endif
     std::string synapse_file(argv[3]);
     read(neuron_file, synapse_file);
   }
   else {
     read(neuron_file);
   }
-#ifdef INSILICO_MPI_ENABLE
   if(insilico::mpi::rank == MASTER) {
-#endif
     std::cerr << "[insilico::configuration::read] SUCCESS: Input file read complete." << '\n';
-#ifdef INSILICO_MPI_ENABLE
   }
-#endif
 }
                  
 // close all output streams
