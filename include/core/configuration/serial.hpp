b93887210ec3f6ec471b87290e5c2d996b794498
Pranav Kulkarni <pranavcode@gmail.com>
2015-04-27 00:22:11 +0530

    [MPI] Reformat error messages.

--- include/core/configuration/serial.hpp
+++ include/core/configuration/serial.hpp
@@ -1,8 +1,10 @@
 /*
- core/configuration/serial.hpp - insilico configuration serial file handling header and source
+ core/configuration/serial.hpp - insilico configuration serial
+                                 file handling source
 
  Copyright (C) 2014 Collins Assisi, Collins Assisi Lab, IISER, Pune
- Copyright (C) 2014-2015 Pranav Kulkarni, Collins Assisi Lab, IISER, Pune <pranavcode@gmail.com>
+ Copyright (C) 2014-2015 Pranav Kulkarni, Collins Assisi Lab,
+                         IISER, Pune <pranavcode@gmail.com>
 
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
@@ -36,16 +38,23 @@
 
 namespace insilico { namespace configuration {
 
+std::string observation_file;
+
 // initialization, check and handle commandline arguments
 void initialize(int argc, char **argv) {
   bool repeat[4] = {false, false, false, false};
-  std::string sargv, output_file, neuron_file, synapse_file, external_current_file;
-  std::string error_msg = "[insilico::configuration::initialize] Supply of file more than once not allowed.\n"; 
+  std::string sargv;
+  std::string output_file, neuron_file, synapse_file;
+  std::string external_current_file;
+  std::string error_msg = "[insilico::configuration::initialize] "
+      "Supply of file more than once not allowed.\n";
   std::string usage_error_msg = "[insilico::configuration::initialize] USAGE: ";
   usage_error_msg += argv[0];
-  usage_error_msg += " -o <output_file.csv> -n <neuron_file.isf> -s <synapse_file.isf> -e <external_file.isfc>\n\n \
-    Options:\n\t-o   Output file\n\t-n   Neuron configuration file\n\t-s   Synapse configuration file (optional)\n \
-    \t-e   External current configuration file (optional)\n\n";
+  usage_error_msg += " -o <output_file.csv> -n <neuron_file.isf> "
+      "-s <synapse_file.isf> -e <external_file.isfc>\n\n "
+      "Options:\n\t-o   Output file\n\t-n   Neuron configuration"
+      " file\n\t-s   Synapse configuration file (optional)\n "
+      "\t-e   External current configuration file (optional)\n\n";
 
   for(int i=1; i < argc; ++i) { sargv += argv[i]; sargv += ' '; }
   std::vector<std::string> cmds = split(sargv, ' ');
@@ -53,58 +62,86 @@ void initialize(int argc, char **argv) {
     std::cerr << usage_error_msg; exit(0);
   }
 
-  std::cerr << "[insilico::configuration::initialize] SUCCESS: Initializing with following parameters:\n";
+  std::cerr << "[insilico::configuration::initialize] "
+      "SUCCESS: Initializing with following parameters:\n";
   for(unsigned iter = 0; iter < cmds.size(); iter+=2) {
     std::string cmd = cmds[iter];
     switch(cmd.at(1)) {
       case 'o':
-        if(repeat[0]) { std::cerr << error_msg << std::endl << usage_error_msg; exit(1); }
+        if(repeat[0]) {
+          std::cerr << error_msg
+                    << std::endl
+                    << usage_error_msg;
+          exit(1);
+        }
         output_file = cmds[iter+1];
         std::cerr << "Output file: " << output_file << '\n';
         repeat[0] = true;
         break;
       case 'n':
-        if(repeat[1]) { std::cerr << error_msg << std::endl << usage_error_msg; exit(1); }
+        if(repeat[1]) {
+          std::cerr << error_msg
+                    << std::endl
+                    << usage_error_msg;
+          exit(1);
+        }
         neuron_file = cmds[iter+1];
         std::cerr << "Neuron file: " << neuron_file << '\n';
         repeat[1] = true;
         break;
       case 's':
-        if(repeat[2]) { std::cerr << error_msg << std::endl << usage_error_msg; exit(1); }
+        if(repeat[2]) {
+          std::cerr << error_msg
+                    << std::endl
+                    << usage_error_msg;
+          exit(1);
+        }
         synapse_file = cmds[iter+1];
         std::cerr << "Synapse file: " << synapse_file << '\n';
         repeat[2] = true;
         break;
       case 'e':
-        if(repeat[3]) { std::cerr << error_msg << std::endl << usage_error_msg; exit(1); }
+        if(repeat[3]) {
+          std::cerr << error_msg
+                    << std::endl
+                    << usage_error_msg;
+          exit(1);
+        }
         external_current_file = cmds[iter+1];
         std::cerr << "External current file: " << external_current_file << '\n';
         repeat[3] = true;
         break;
       default:
-        std::cerr << "Default error: Unknown argument " << cmd << std::endl;
+        std::cerr << "Default error: Unknown argument " << cmd << '\n';
         std::cerr << usage_error_msg; exit(0);
     }
   }
-  if(repeat[0]) { outstream.open(output_file, std::ios::out); }
+  if(repeat[0]) {
+    observation_file = output_file;
+    outstream.open(output_file, std::ios::out);
+  }
   else {
-    std::cerr << "[insilico::configuration::initialize] Output file is required.\n"; 
+    std::cerr << "[insilico::configuration::initialize]"
+        " Output file is required.\n";
     exit(1);
   }
   if(repeat[2]) { read(neuron_file, synapse_file); }
   else if(repeat[1]) { read(neuron_file); }
   else {
-    std::cerr << "[insilico::configuration::initialize] Neuron configuration file is required.\n";
+    std::cerr << "[insilico::configuration::initialize]"
+        " Neuron configuration file is required.\n";
     exit(1);
   }
   if(repeat[3]) { injector::read(external_current_file); }
-  std::cerr << "[insilico::configuration::read] SUCCESS: Input file read complete." << '\n';
+  std::cerr << "[insilico::configuration::read]"
+      " SUCCESS: Input file read complete.\n";
 }
 
 // close all output streams
 void finalize() {
   outstream.close();
-  std::cerr << "[insilico::configuration::finalize] SUCCESS: Releasing resources."<<'\n';
+  std::cerr << "[insilico::configuration::finalize]"
+      " SUCCESS: Releasing resources."<<'\n';
 }
 
 } } // namespace insilico::configuration
