969e709a426fc264d9642ec01834f96fc0985067
Pranav Kulkarni <pranavcode@gmail.com>
2015-05-27 04:20:03 +0530

    Licence correction.

--- include/insilico/core/engine/generate.hpp
+++ include/insilico/core/engine/generate.hpp
@@ -1,7 +1,8 @@
 /*
   core/engine/generate.hpp - Type generation and management
 
-  Copyright (C) 2015 Pranav Kulkarni, Collins Assisi Lab, IISER, Pune <pranavcode@gmail.com>
+  Copyright (C) 2015 Pranav Kulkarni, Collins Assisi Lab,
+                     IISER, Pune <pranavcode@gmail.com>
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
