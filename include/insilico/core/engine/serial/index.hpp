67d52138db02b3d3d5cec4e882394e5d4456ea3a
Pranav Kulkarni <pranavcode@gmail.com>
2015-06-01 02:05:10 +0530

    [Engine]{optimization} Value pre-store.

--- include/insilico/core/engine/serial/index.hpp
+++ include/insilico/core/engine/serial/index.hpp
@@ -109,12 +109,14 @@ auto get_synapse_indices(std::string _variable) -> std::vector<unsigned> {
 
 auto get_indices(std::string _variable) -> std::vector<unsigned> {
   std::vector<unsigned> indices;
+  auto neuron_indices = get_neuron_indices(_variable);
+  auto synapse_indices = get_synapse_indices(_variable);
   indices.insert(indices.end(),
-                 get_neuron_indices(_variable).begin(),
-                 get_neuron_indices(_variable).end());
+                 neuron_indices.begin(),
+                 neuron_indices.end());
   indices.insert(indices.end(),
-                 get_synapse_indices(_variable).begin(),
-                 get_synapse_indices(_variable).end());
+                 synapse_indices.begin(),
+                 synapse_indices.end());
   return indices;
 }
 
